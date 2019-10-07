3d7b48c7081bf0334104626c417a5343801ae7ef
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-07 16:20:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,17 @@
-def greeting_me ("soner","Ruby")
-  puts "Hi my name is #{name}.I am learning to program in #{language} "
-  greeting_me =("soner","Ruby")
+ def introduction(name)
+puts "Hi, my name is #{name}."
 end
+introduction("soner")
 
+ def introduction_with_language(name,language)
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("soner","Ruby")
+
+def introduction_with_language_optional(name,language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
+  
 
 
 
